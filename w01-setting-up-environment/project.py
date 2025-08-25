53b48d760610417a3fd792539a8855ac1e0ea431
Nadege Lemperiere <nadege.lemperiere@gmail.com>
2025-08-25 17:54:08 -0400

    W01 : first version to check codespace integration

---
+++ w01-setting-up-environment/project.py
@@ -0,0 +1,5 @@
+#################################################################
+##### Project: Setting Up Environment
+#################################################################
+
+# Write here your "Hello World" to validate your codespace environment
